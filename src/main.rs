da102e4411e9dfa0a06df71588378cb446aa354b
Shuocheng Wang <wangshch5@outlook.com>
2023-12-01 23:19:54 +0800

    Dynamic attributes

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,40 @@ use leptos::*;
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
+    let (x, set_x) = create_signal(0);
+    let (y, set_y) = create_signal(0);
+    let double_count = move || count() * 2;
 
     view! {
-        <button on:click=move |_| {
-            set_count.update(|n| *n += 1);
-        }>"click me: " {count}</button>
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+
+            class:red=move || count() % 2 == 1
+        >
+            "click me: "
+            {count}
+        </button>
+
+        <div
+            style="position: absolute"
+            style:left=move || format!("{}px", x() + 100)
+            style:top=move || format!("{}px", y() + 100)
+            style:background-color=move || format!("rgb({}, {}, 100)", 100 + x(), 100 + y())
+            style=("--columns", x)
+        >
+            <button on:click=move |_| {
+                set_x.update(|x| *x += 1);
+            }>"Increase X"</button>
+            <button on:click=move |_| {
+                set_y.update(|y| *y += 1);
+            }>"Increase Y"</button>
+            "Move when coordinates change"
+        </div>
+
+        <progress max="50" value=double_count></progress>
+        <p>"Double count: " {double_count}</p>
     }
 }
 
