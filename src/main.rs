216e4e24b27e7feb49bdb25d6e8b194320e43c49
Shuocheng Wang <wangshch5@outlook.com>
2023-12-25 15:05:08 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-#![allow(non_snake_case, unused)]
+#![allow(non_snake_case)]
+// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
 use dioxus::prelude::*;
-use dioxus_fullstack::prelude::*;
 
 fn main() {
-    LaunchBuilder::new(app).launch();
+    // launch the web app
+    dioxus_web::launch(App);
 }
 
-fn app(cx: Scope) -> Element {
-    let mut count = use_state(cx, || 0);
-
+// create a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
     cx.render(rsx! {
-        h1 { "High-Five counter: {count}" }
-        button { onclick: move |_| count += 1, "Up high!" }
-        button { onclick: move |_| count -= 1, "Down low!" }
+        div {
+            "Hello, world!"
+        }
     })
 }
-
