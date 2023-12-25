32e189b6b237de64f711d8517ea2085db59eb4a3
Shuocheng Wang <wangshch5@outlook.com>
2023-12-25 15:38:57 +0800

    Add local_storage

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 // import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
 use dioxus::prelude::*;
 
+mod store;
+
+use crate::store::Store;
+
 fn main() {
     // launch the web app
     dioxus_web::launch(App);
@@ -9,6 +13,8 @@ fn main() {
 
 // create a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
+    let store = Store::new("lose-fat").unwrap();
+    
     cx.render(rsx! {
         div {
             "Hello, world!"
---
+++ src/store.rs
@@ -0,0 +1,22 @@
+use wasm_bindgen::prelude::*;
+
+pub struct Store {
+    local_storage: web_sys::Storage,
+    name: String,
+}
+
+
+impl Store {
+    pub fn new(name: &str) -> Option<Store> {
+        let window = web_sys::window()?;
+        if let Ok(Some(local_storage)) = window.local_storage() {
+            let mut store = Store {
+                local_storage,
+                name: String::from(name),
+            };
+            Some(store)
+        } else {
+            None
+        }
+    }
+}
