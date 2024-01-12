a15a93aa5a23e102aed6048e068d1c2d57aaf5a0
Shuocheng Wang <wangshch5@outlook.com>
2024-01-12 15:16:25 +0800

    Add router

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,50 @@
 #![allow(non_snake_case)]
 use dioxus::prelude::*;
+use dioxus_router::prelude::*;
 
 mod store;
 
 use crate::store::Store;
 
-fn main() {
-    dioxus_web::launch(App);
+#[derive(Routable, Clone, PartialEq)]
+enum Route {
+    #[layout(Nav)]
+    #[route("/")]
+    Homepage {},
+
+    #[route("/food-list")]
+    FoodList {},
+}
+
+#[component]
+fn Homepage(cx: Scope) -> Element {
+    render! { h1 { "Welcome home" } }
+}
+
+#[component]
+fn FoodList(cx: Scope) -> Element {
+    render! { h1 { "Food list" } }
+}
+
+#[component]
+fn Nav(cx: Scope) -> Element {
+    render! {
+       nav {
+           li { Link { to: Route::Homepage {}, "Home" } }
+           li { Link { to: Route::FoodList {}, "Food List"} }
+       }
+       div { Outlet::<Route> {} }
+    }
 }
 
 fn App(cx: Scope) -> Element {
     let store = Store::new("lose-fat").unwrap();
 
     cx.render(rsx! {
-        div {
-            "Hello, world!"
-        }
+        Router::<Route> {}
     })
+}
+
+fn main() {
+    dioxus_web::launch(App);
 }
